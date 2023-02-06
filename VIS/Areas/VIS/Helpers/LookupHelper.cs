ed2eed263e16d81812f97ce79e6ad0f6143b8145
liveserver <live@viennasolutions.com>
2023-02-06 02:59:07 +0000

    Added white space

--- VIS/Areas/VIS/Helpers/LookupHelper.cs
+++ VIS/Areas/VIS/Helpers/LookupHelper.cs
@@ -283,7 +283,7 @@ namespace VIS.Classes
             if (aliasName != "")
             {
                 sql = sql.Replace(lInfo.tableName + ".", aliasName + ".");
-                sql = sql.Replace("FROM " + lInfo.tableName, "FROM " + lInfo.tableName + " " + aliasName);
+                sql = sql.Replace("FROM " + lInfo.tableName+ " ", "FROM " + lInfo.tableName + " " + aliasName+" ");
             }
 
             var posFrom = sql.IndexOf(" FROM ");
