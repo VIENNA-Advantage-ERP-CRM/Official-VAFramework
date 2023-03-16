d6c6d6914663cd690a8944947a3bd83bb19cc7d3
Pre Live <preliveserver@viennasolutions.com>
2023-03-16 07:43:21 +0000

    fix

--- VAModelAD/Common/Common.cs
+++ VAModelAD/Common/Common.cs
@@ -1463,7 +1463,7 @@ namespace VAdvantage.Common
 
                     if (!string.IsNullOrEmpty(columnName))
                     {
-                        sql = "SELECT " + columnName + ", COUNT(NVL(" + columnName + ",0)) AS GroupCount " + SQLWhereCond + " GROUP BY " + columnName;
+                        sql = "SELECT " + columnName + ", COUNT(NVL(" + columnName + ",'0')) AS GroupCount " + SQLWhereCond + " GROUP BY " + columnName;
 
                         dr = DB.ExecuteReader(sql);
                         while (dr.Read())
