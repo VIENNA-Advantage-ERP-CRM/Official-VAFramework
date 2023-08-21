c4941cfc2c514305649e745078274aa7721a1b9d
mandeep.va@outlook.com <mandeep.va@outlook.com>
2023-08-21 15:10:04 +0530

    Axios Implementations

---
+++ VIS/Areas/VIS/Scripts/react/Component/Grid.jsx
@@ -0,0 +1,8 @@
+﻿function Welcome(props) {
+    return (<>
+        <h2>Welcome {props.name}</h2>
+    </>);
+}
+
+export default Welcome;
+
--- VIS/Areas/VIS/Scripts/react/Component/TestComponent.jsx
+++ VIS/Areas/VIS/Scripts/react/Component/TestComponent.jsx
@@ -1,41 +1,100 @@
-﻿function Component(props) {
-   // console.log(props);
-    return (<>
-        <form className="container-fluid">
-            <div className="form-group">
-                <label htmlFor="exampleInputEmail1">Email address</label>
-                <input
-                    type="email"
-                    className="form-control"
-                    id="exampleInputEmail1"
-                    aria-describedby="emailHelp"
-                    placeholder="Enter email"
-                />
-                <small id="emailHelp" className="form-text text-muted">
-                    We'll never share your email with anyone else.
-                </small>
-            </div>
-            <div className="form-group">
-                <label htmlFor="exampleInputPassword1">Password</label>
-                <input
-                    type="password"
-                    className="form-control"
-                    id="exampleInputPassword1"
-                    placeholder="Password"
-                />
-            </div>
-            <div className="form-group form-check">
-                <input type="checkbox" className="form-check-input" id="exampleCheck1" />
-                <label className="form-check-label" htmlFor="exampleCheck1">
-                    Check me out
-                </label>
-            </div>
-            <button type="submit" className="btn btn-primary">
-                Submit
-            </button>
-        </form>
+﻿//const Grid = React.lazy(() => import('./Grid.jsx'));
 
-    </>);
+
+class GetPost extends React.Component {
+    constructor(props) {
+        super(props);
+        this.props = props;
+    }
+    render() {
+        return (<table className="table table-bordered table-hover">
+            <thead>
+                <tr>
+                    <th>userId</th>
+                    <th>title</th>
+                    <th>body</th>
+                    <th>Delete</th>
+                </tr>
+            </thead>
+            <tbody>
+                {
+                    this.props.posts.map((post, index) => {
+                  return <tr key={index}>
+                        <td>{post.userId}</td>
+                        <td>{post.title}</td>
+                      <td>{post.body}</td>
+                      <td onClick={() => this.props.tableRowRemove(index)}>Delete</td>
+                        </tr>
+                    })
+                }
+            </tbody>
+        </table>);
+    }
 }
 
-export default Component;
+class AddProduct extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            name: 'Mandeep',
+            email: '',
+            phone: '',
+            ProductData: []
+        };
+    };
+
+   
+
+    componentDidMount() {
+        axios.get("https://jsonplaceholder.typicode.com/posts").then(response => {
+            console.log(response.data);  
+            this.setState({
+                ProductData: response.data
+            });
+        });
+    }
+
+    handleChange = (event) => {
+        this.setState({ [event.target.name]: event.target.value });
+    }
+
+    handleSubmit = (event) => {
+        event.preventDefault();
+        console.log(this.state);
+    }
+
+    tableRowRemove = (index) => {
+        const posts = this.state.ProductData;
+        posts.splice(index, 1);
+        this.setState({
+            ProductData: posts
+        })
+    };
+    
+    render() {
+        return (<>
+            <div className="container">
+                <form onSubmit={this.handleSubmit}>
+                    <div className="form-group">
+                        <label>Name</label>
+                        <input type="text" className="form-control" value={this.state.name} name="name" onChange={this.handleChange} />
+                    </div>
+                    <div className="form-group">
+                        <label>Email</label>
+                        <input type="email" className="form-control" value={this.state.email} name="email" onChange={this.handleChange}  />
+                    </div>
+                    <div className="form-group">
+                        <label>Phone</label>
+                        <input type="tel" className="form-control" value={this.state.phone} name="phone" onChange={this.handleChange}  />
+                    </div>
+                    <button type="submit" className="btn btn-primary btn-block">Submit</button>
+                </form>
+                <hr />
+                <GetPost posts={this.state.ProductData} tableRowRemove={this.tableRowRemove} />
+            </div>
+
+        </>);
+    }
+}
+
+export default AddProduct;
