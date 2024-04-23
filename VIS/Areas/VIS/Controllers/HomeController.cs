a561d3df1a2ee2cfca4feccd575e0e33a641adfd
harwinder <harwinder.singh@viennasolutions.com>
2024-04-23 13:20:38 +0530

    logindf

--- VIS/Areas/VIS/Controllers/HomeController.cs
+++ VIS/Areas/VIS/Controllers/HomeController.cs
@@ -46,7 +46,7 @@ namespace VIS.Controllers
         HomeHelper objHomeHelp = null;
 
         private static bool isBundleAdded = false;
-        private ReaderWriterLockSlim _lockSlim = new ReaderWriterLockSlim(LockRecursionPolicy.SupportsRecursion);
+        //private ReaderWriterLockSlim _lockSlim = new ReaderWriterLockSlim(LockRecursionPolicy.SupportsRecursion);
 
 
 
@@ -72,16 +72,6 @@ namespace VIS.Controllers
         public ActionResult Index(FormCollection form)
         {
 
-
-            //if (LoginHelper.IsSiteUnderMaintenance())
-            //{
-            //    if (User.Identity.IsAuthenticated)
-            //    {
-            //        return new AccountController().SignOff(Session["ctx"] as Ctx);
-            //    }
-            //    return View("Maintenance"); 
-            //}
-
             if (Request.QueryString.Count > 0)
             {
                 // string user = Request.QueryString["U"];
@@ -95,19 +85,6 @@ namespace VIS.Controllers
                 // ac.SetAuthCookie(md, Response); //AutoLogin if all passed
                 // return RedirectToAction("Index");
             }
-
-            //if (!User.Identity.IsAuthenticated)
-            //{
-            //    // Required to allow javascript redirection through to browser
-            //    this.Response.TrySkipIisCustomErrors = true;
-            //    this.Response.Status = "401 Unauthorized";
-            //    this.Response.StatusCode = 401;
-            //    // note that the following line is .NET 4.5 or later only
-            //    // otherwise you have to suppress the return URL etc manually!
-            //    this.Response.SuppressFormsAuthenticationRedirect = true;
-            //    // If we got this far, something failed
-
-            //}
 
 
             var url = CloudLogin.IsAllowedToLogin(Request.Url.ToString());
@@ -139,8 +116,7 @@ namespace VIS.Controllers
                 }
 
 
-                //AccountController a = new AccountController();
-                //a.LogOff();
+                
                 FormsIdentity ident = User.Identity as FormsIdentity;
                 Ctx ctx = null;
                 if (ident != null)
@@ -150,28 +126,16 @@ namespace VIS.Controllers
                     LoginContext lCtx = JsonHelper.Deserialize(loginContextString, typeof(LoginContext)) as LoginContext;
                     IDataReader dr = null;
 
-
-
-
-
                     //create class from string  
                     string key = "";
                     if (Session["ctx"] != null)
                     {
                         ctx = Session["ctx"] as Ctx;
 
-                        //Update Old Session
-                        //MSession session = MSession.Get(ctx, false);
-                        //if (session != null)
-                        //    session.Logout();
-
                         key = ctx.GetSecureKey();
-
-                        //if (Session.Timeout < 2)
-                        //{
+                       
                         SessionEventHandler.SessionEnd(ctx);
                         Session.Timeout = 17;
-                        //}
                         Session["ctx"] = null;
 
                     }
@@ -187,12 +151,9 @@ namespace VIS.Controllers
 
 
                     //if not system admin
-                    if (LoginHelper.IsSiteUnderMaintenance() && ctx.GetAD_Role_ID() != 0)
+                    if (ctx.GetAD_Role_ID() != 0 &&
+                        LoginHelper.IsSiteUnderMaintenance())
                     {
-                        // if (User.Identity.IsAuthenticated)
-                        //{
-                        //  return new AccountController().SignOff(ctx);
-                        //}
                         return View("Maintenance");
                     }
 
@@ -201,13 +162,6 @@ namespace VIS.Controllers
                         ctx.SetSecureKey(key);
                     }
                     Session["ctx"] = ctx;
-
-
-                    //Check For 
-
-
-
-
 
                     //get login Language object on server
                     var loginLang = ctx.GetAD_Language();
