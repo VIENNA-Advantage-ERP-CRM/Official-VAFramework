6cf687335c6a6b35d2e027ab2a212e66b9cade4c
harwinder <harwinder.singh@viennasolutions.com>
2024-05-17 19:07:49 +0530

    handle table not exist case

--- VAModelAD/Model/POValidator.cs
+++ VAModelAD/Model/POValidator.cs
@@ -198,7 +198,7 @@ namespace VAModelAD.Model
             {
 
                 //Pick ModuleID from AutoMarking Configuration window
-                int curRefModID = DB.GetSQLValue(null,
+                int curRefModID = DB.GetSQLValue(po.Get_Trx(),
                     "SELECT VA093_RefModule_ID FROM  VA093_AutoMarkingConfig WHERE Processed='N' AND IsActive='Y' AND AD_Role_ID="
                     + po.GetCtx().GetAD_Role_ID());
                 if (curRefModID > 0)
