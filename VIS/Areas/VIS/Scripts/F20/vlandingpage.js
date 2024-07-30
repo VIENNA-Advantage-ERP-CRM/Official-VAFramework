830932b5325c626cb544396cea064f831191df16
Ruby <ruby.devi@viennasolutions.com>
2024-07-30 18:38:54 +0530

    add parameter

--- VIS/Areas/VIS/Scripts/F20/vlandingpage.js
+++ VIS/Areas/VIS/Scripts/F20/vlandingpage.js
@@ -432,7 +432,7 @@
                 $div.append(widget.items).append('<div class="linktitle">' + widget.DisplayName + '</div>');
                 $item.append($div);
             } else if (widget.Type == "C" || widget.Type == "K" || widget.Type == "V") {
-                VADB.chartFactory.getChart(widget.WidgetID, $item, widget.Type);
+                VADB.chartFactory.getChart(widget.WidgetID, $item, widget.Type, info);
             }
 
             var trash = $('<div class="vis-widgetDelete"><i class="fa fa-trash-o"></i><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 16 16"><path transform="rotate(90 8 8)" d="M5 8a1 1 0 11-2 0 1 1 0 012 0zm4 0a1 1 0 11-2 0 1 1 0 012 0zm3 1a1 1 0 100-2 1 1 0 000 2z"></path></svg></div>');
