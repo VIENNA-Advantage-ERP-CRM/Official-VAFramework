363199dec3b6a979b28b8ccc294f645bbddb0342
RubyVIS <131639742+RubydeviVIS@users.noreply.github.com>
2024-09-19 15:39:22 +0530

    provide all role access to widget (#301)
    * Chat in tab Panel
    
    * add down icon
    
    * remove alert

---
+++ VAModelAD/ModelAD/MWidgetAccess.cs
@@ -0,0 +1,37 @@
+﻿using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VAdvantage.DataBase;
+using VAdvantage.Utility;
+using ViennaAdvantage.Model;
+
+namespace VAdvantage.Model
+{
+    public class MWidgetAccess : X_AD_Widget_Access
+    {
+        public MWidgetAccess(Ctx ctx, DataRow rs, Trx trxName)
+          : base(ctx, rs, trxName)
+        {
+        }
+
+        public MWidgetAccess(Ctx ctx, int AD_Widget_ID, Trx trxName)
+           : base(ctx, AD_Widget_ID, trxName)
+        {
+
+        }
+
+        /// <summary>
+        ///Parent Constructor
+        /// </summary>
+        /// <param name="parent">parent</param>
+        /// <param name="AD_Role_ID">role id</param>
+        public MWidgetAccess(MWidget parent, int AD_Role_ID)
+            : base(parent.GetCtx(), 0, parent.Get_TrxName())
+        {
+            SetClientOrg(parent);
+            SetAD_Widget_ID(parent.GetAD_Widget_ID());
+            SetAD_Role_ID(AD_Role_ID);
+        }
+    }
+}
