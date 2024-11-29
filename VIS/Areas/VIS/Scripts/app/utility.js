7b5d9a452ed5c4b9602bf58a8b48b1c8e3ad683a
harwinder <harwinder.singh@viennasolutions.com>
2024-11-29 19:11:00 +0530

    login

--- VIS/Areas/VIS/Scripts/app/utility.js
+++ VIS/Areas/VIS/Scripts/app/utility.js
@@ -318,14 +318,18 @@
         },
 
         getFunctionByName: function (functionName, context /*, args */) {
-
-            var args = [].slice.call(arguments).splice(2);
-            var namespaces = functionName.split(".");
-            var func = namespaces.pop();
-            for (var i = 0; i < namespaces.length; i++) {
-                context = context[namespaces[i]];
+            try {
+                var args = [].slice.call(arguments).splice(2);
+                var namespaces = functionName.split(".");
+                var func = namespaces.pop();
+                for (var i = 0; i < namespaces.length; i++) {
+                    context = context[namespaces[i]];
+                }
+                return context[func];
             }
-            return context[func];
+            catch {
+                return null;
+            }
         },
 
         utf8_to_b64: function (str) {
