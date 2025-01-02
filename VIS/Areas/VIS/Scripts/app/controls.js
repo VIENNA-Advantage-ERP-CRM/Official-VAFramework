a7bad7da7a88feb19bdc992a8e2696d3ce55e37c
Harwinder-VA <54847766+Harwinder-VA@users.noreply.github.com>
2025-01-02 17:12:09 +0530

    dynamic fg, space action grp (#369)

--- VIS/Areas/VIS/Scripts/app/controls.js
+++ VIS/Areas/VIS/Scripts/app/controls.js
@@ -1449,6 +1449,22 @@
         this.setBackground(false);
     };
 
+    VButton.prototype.setVisible = function (visible) {
+        this.visible = visible;
+        //Action group[]
+        var isag = this.ctrl.parent().length>0 && this.ctrl.parent()[0].tag == "LI";
+        if (visible) {
+            this.ctrl.show();
+            if (isag)
+                this.ctrl.parent().show();
+        } else {
+            this.ctrl.hide();
+            if (isag);
+            this.ctrl.parent().hide();
+        }
+    };
+
+
     /**
      *	Return Value
      *  @return value
