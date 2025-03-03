3448c1038f9d69b9513cd03ad47d9eae3384c17a
harpreetVienna <148218239+harpreetVienna@users.noreply.github.com>
2025-03-03 12:45:51 +0530

    Worked on Follow Up, Assigned Record & Pending Checklist widget. (#413)
    * Worked on Follow Up, Assigned Record & Pending Checklist widget.
    
    * Worked on RTL of Pending Checklist & Assign Record widgets
    
    * Worked on correcting data in Pending Checklist & UI modification while editing the Assign record widget.
    
    ---------
    
    Co-authored-by: harpreet.singh <harpreet.singh@VA-MOH-RENT-35>

--- VIS/Areas/VIS/Controllers/Subscribe/SubscribeController.cs
+++ VIS/Areas/VIS/Controllers/Subscribe/SubscribeController.cs
@@ -12,28 +12,90 @@ namespace VIS.Controllers
 {
     public class SubscribeController : Controller
     {
-        //
-        // GET: /VIS/Subscribe/
+        /// <summary>
+        /// Insert Multiple Subscription
+        /// </summary>
+        /// <param name="AD_Window_ID"></param>
+        /// <param name="Record_ID"></param>
+        /// <param name="AD_Table_ID"></param>
+        /// <returns></returns>
+        [AjaxAuthorizeAttribute]
+        [AjaxSessionFilterAttribute]
+        public JsonResult MultipleSubscribe(int AD_Window_ID, List<int> Record_ID, int AD_Table_ID)
+        {
+            Ctx ct = Session["ctx"] as Ctx;
+            SubscribeModel model = new SubscribeModel(ct);
+            var result = model.InsertMultipleSubscription(ct, AD_Window_ID, Record_ID, AD_Table_ID);
+            return Json(JsonConvert.SerializeObject(result), JsonRequestBehavior.AllowGet);
+        }
         [AjaxAuthorizeAttribute]
         [AjaxSessionFilterAttribute]
         public JsonResult Subscribe(int AD_Window_ID, int Record_ID, int AD_Table_ID)
         {
-             Ctx ct = Session["ctx"] as Ctx;
-             SubscribeModel model = new SubscribeModel(ct);
-             var result= model.InsertSubscription(AD_Window_ID,Record_ID,AD_Table_ID);
-             return Json(JsonConvert.SerializeObject(result), JsonRequestBehavior.AllowGet);
+            Ctx ct = Session["ctx"] as Ctx;
+            SubscribeModel model = new SubscribeModel(ct);
+            var result = model.InsertSubscription(AD_Window_ID, Record_ID, AD_Table_ID);
+            return Json(JsonConvert.SerializeObject(result), JsonRequestBehavior.AllowGet);
         }
+        /// <summary>
+        /// UnSubscribeMultiple
+        /// </summary>
+        /// <param name="AD_Window_ID"></param>
+        /// <param name="Record_IDs"></param>
+        /// <param name="AD_Table_ID"></param>
+        /// <returns></returns>
 
-
+        [AjaxAuthorizeAttribute]
+        [AjaxSessionFilterAttribute]
+        public JsonResult UnSubscribeMultiple(int AD_Window_ID, List<int> Record_IDs, int AD_Table_ID)
+        {
+            Ctx ct = Session["ctx"] as Ctx;
+            SubscribeModel model = new SubscribeModel(ct);
+            var result = model.DeleteMultipleSubscription(ct, AD_Window_ID, Record_IDs, AD_Table_ID);
+            return Json(JsonConvert.SerializeObject(result), JsonRequestBehavior.AllowGet);
+        }
         [AjaxAuthorizeAttribute]
         [AjaxSessionFilterAttribute]
         public JsonResult UnSubscribe(int AD_Window_ID, int Record_ID, int AD_Table_ID)
         {
             Ctx ct = Session["ctx"] as Ctx;
             SubscribeModel model = new SubscribeModel(ct);
-            var result = model.DeleteSubscription(AD_Window_ID,Record_ID,AD_Table_ID);
+            var result = model.DeleteSubscription(AD_Window_ID, Record_ID, AD_Table_ID);
             return Json(JsonConvert.SerializeObject(result), JsonRequestBehavior.AllowGet);
         }
-       
-	}
+
+        /// <summary>
+        /// Subcribe all records
+        /// </summary>
+        /// <param name="AD_Window_ID"></param>
+        /// <param name="AD_Tab_ID"></param>
+        /// <param name="AD_Table_ID"></param>
+        /// <param name="AD_Record_ID"></param>
+        /// <returns></returns>
+        [AjaxAuthorizeAttribute]
+        [AjaxSessionFilterAttribute]
+        public JsonResult SubscribeAll(int AD_Window_ID, int AD_Tab_ID, int AD_Table_ID, int AD_Record_ID)
+        {
+            Ctx ctx = Session["ctx"] as Ctx;
+            SubscribeModel model = new SubscribeModel();
+            var result = model.SubscribeAll(ctx, AD_Window_ID, AD_Tab_ID, AD_Table_ID, AD_Record_ID);
+            return Json(JsonConvert.SerializeObject(result), JsonRequestBehavior.AllowGet);
+        }
+
+        /// <summary>
+        /// Get Unread message count
+        /// </summary>
+        /// <returns></returns>
+        public JsonResult UnreadMessageCount()
+        {
+            dynamic result = 0;
+            if (Session["ctx"] != null)
+            {
+                var ctx = Session["ctx"] as Ctx;
+                SubscribeModel obj = new SubscribeModel();
+                result = obj.UnreadMessageCount(ctx);
+            }
+            return Json(JsonConvert.SerializeObject(result), JsonRequestBehavior.AllowGet);
+        }
+    }
 }
---
+++ VIS/Areas/VIS/Scripts/app/Framework/subscribe.js
@@ -0,0 +1,149 @@
+﻿/************************************************************
+* Module Name    : VIS
+* Purpose        : Subscribe/Unsubscribe records
+* chronological  : Development
+* Created Date   : 27 January 2025
+* Created by     : (VAI049)
+***********************************************************/
+; VIS = window.VIS || {};
+
+(function (VIS, $) {
+    function subscribe(record_ID, reloadSubscribe, self) {
+        var $root_subs = null;
+        var subscribedItems = self.curTab._subscribe;
+
+        reloadSubscribe();
+
+        // Get matched items
+        const matchedItems = subscribedItems.filter(item =>
+            record_ID.includes(Number(item.ID)) // Match record_ID with subscribed item ID
+        );
+
+        // Define UI logic based on matched and unmatched items
+        if (record_ID.length < 2) {
+            if (matchedItems.length) {
+                // If there are matched items, show unsubscribe and allRecords options
+                $root_subs = $(
+                    '<div>' +
+                        '<ul class="vis-apanel-rb-ul">' +
+                            '<li data-id="unsubscribe" style="margin-right: 10px;">' + VIS.Msg.getMsg("VIS_Unsubscribe") + '</li>' +
+                            '<li data-id="allRecords" style="margin-right: 10px;">' + VIS.Msg.getMsg("VIS_AllRecords") + '</li>' +
+                        '</ul>' +
+                    '</div>'
+                );
+            }
+            else {
+                // If no matched items, show selectedRecords and allRecords options
+                $root_subs = $('<div>' +
+                        '<ul class="vis-apanel-rb-ul">' +
+                            '<li data-id="selectedRecords" style="margin-right: 10px;">' + VIS.Msg.getMsg("VIS_SelectedRecords") + '</li>' +
+                            '<li data-id="allRecords" style="margin-right: 10px;">' + VIS.Msg.getMsg("VIS_AllRecords") + '</li>' +
+                        '</ul>' +
+                    '</div>'
+                );
+            }
+        }
+        else {
+            // For cases where there are multiple records, show all options
+            $root_subs = $('<div>' +
+                    '<ul class="vis-apanel-rb-ul">' +
+                        '<li data-id="selectedRecords" style="margin-right: 10px;">' + VIS.Msg.getMsg("VIS_SelectedRecords") + '</li>' +
+                        '<li data-id="allRecords" style="margin-right: 10px;">' + VIS.Msg.getMsg("VIS_AllRecords") + '</li>' +
+                        '<li data-id="unsubscribe" style="margin-right: 10px;">' + VIS.Msg.getMsg("VIS_Unsubscribe") + '</li>' +
+                    '</ul>' +
+                '</div>'
+            );
+        }
+
+        // Attach a click handler to each <li> item
+        $root_subs.find('li').off('click');
+        $root_subs.find('li').on('click', function (e) {
+            self.setBusy(true);
+            e.stopImmediatePropagation();  // Prevent any other click events from being triggered
+            var overlay = $('#w2ui-overlay-main');
+            overlay.remove(); // Close the overlay
+
+            var itemId = $(this).data('id');  // Get the data-id of the clicked <li>
+
+            var url;
+            var data;
+
+            // Handle different actions based on which option is clicked
+            if (itemId == "unsubscribe") {
+                url = VIS.Application.contextUrl + 'Subscribe/UnSubscribeMultiple';
+                data = { AD_Window_ID: self.curTab.getAD_Window_ID(), Record_IDs: record_ID, AD_Table_ID: self.curTab.getAD_Table_ID() };
+            }
+            else if (itemId == "allRecords") {
+                url = VIS.Application.contextUrl + 'Subscribe/SubscribeAll';
+                data = { AD_Window_ID: self.curTab.getAD_Window_ID(), AD_Tab_ID: self.curTab.getAD_Tab_ID(), AD_Table_ID: self.curTab.getAD_Table_ID(), AD_Record_ID: self.curTab.getRecord_ID() };
+            }
+            else if (itemId == "selectedRecords") {
+                url = VIS.Application.contextUrl + 'Subscribe/MultipleSubscribe';
+                data = { AD_Window_ID: self.curTab.getAD_Window_ID(), Record_ID: record_ID, AD_Table_ID: self.curTab.getAD_Table_ID() };
+            }
+
+            // AJAX call to handle the subscribe/unsubscribe actions
+            $.ajax({
+                url: url,
+                type: 'POST',
+                dataType: 'JSON',
+                data: data,
+                success: function (result) {
+                    if (result) {
+                        self.setBusy(false);
+                        if (typeof result === 'string') {
+                            result = JSON.parse(result);
+                        }
+                        if (url == VIS.Application.contextUrl + 'Subscribe/UnSubscribeMultiple' && result < 1) {
+                            VIS.ADialog.error("VIS_RecordNotUnSubscribed");
+                            return;
+                        }
+                        if ((url == VIS.Application.contextUrl + 'Subscribe/SubscribeAll' && (result == 1 || result == 2)) ||
+                            (url == VIS.Application.contextUrl + 'Subscribe/UnSubscribeMultiple' && result > 0) ||
+                            (url == VIS.Application.contextUrl + 'Subscribe/MultipleSubscribe' && result > 0)) {
+                            reloadSubscribe();
+                        }
+                        else {
+                            VIS.ADialog.error("VIS_RecordNotSubscribed");
+                        }
+                    } 
+                },
+                error: function (r) {
+                    VIS.ADialog.error(VIS.Msg.getMsg("Error") + r.statusText);
+                    self.setBusy(false);
+                }
+            });
+        });
+
+        // Show the overlay with the cloned content
+        self.aSubscribe.getListItmIT().w2overlay($root_subs.clone(true), { css: { height: '200px', width: '100px' } });
+
+        this.disposeComponent = function () {
+            // Cleanup logic, if needed
+            $root_subs = null;
+        };
+
+        // Get design from root
+        this.getRoot = function () {
+            return $root;
+        };
+
+    };
+
+    // init method called on loading a form
+    subscribe.prototype.init = function (windowNo, frame) {
+        this.frame = frame;
+        this.windowNo = windowNo;
+        this.Initalize();
+        this.frame.getContentGrid().append(this.getRoot());
+    };
+
+    subscribe.prototype.dispose = function () {
+        this.disposeComponent();
+        if (this.frame) this.frame.dispose();
+        this.frame = null;
+    };
+
+    VIS.subscribe = subscribe;
+
+})(VIS, jQuery);
