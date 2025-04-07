7fe45e1f6e3a36604c9c51e94fad8148216c5364
sukhvinder008 <148348917+sukhvinder008@users.noreply.github.com>
2025-04-07 16:16:25 +0530

    1.Changes in Api key Section (#436)

--- VIS/Areas/VIS/Scripts/app/Framework/VIS_SecretKeyDialog.js
+++ VIS/Areas/VIS/Scripts/app/Framework/VIS_SecretKeyDialog.js
@@ -12,7 +12,7 @@
         var ch = null;
         var $bsyDiv = $('<div class="vis-busyindicatorouterwrap"><div class="vis-busyindicatorinnerwrap"><i class="vis-busyindicatordiv"></i></div></div>');
 
-        var $root = $('<div>');
+        var $root = $('<div class="h-100">');
         var $self = this;
         this.windowNo;
         this.onClose = null;
@@ -20,14 +20,14 @@
         // HTML for saving the key after password confirmation
 
         var saveKeyHtml =
-            '<div class="">' +
+            '<div class="VIS-key-popup-container">' +
             //'<h2>' + VIS.Msg.getMsg("VAAPI_SaveYourKey") + '</h2>' +
-            '<p>' + VIS.Msg.getMsg("VAAPI_SaveYourKeyInfo") + '</p>' +
+            '<p class="VIS-secret-key-p-content">' + VIS.Msg.getMsg("VAAPI_SaveYourKeyInfo") + '</p>' +
             '<div class="VIS-save-secret-key-key-container">' +
             '<input id="VIS_SecretKeytxt_' + windowNo + '" type="text" value="" readonly>' +
-            '<button id="VIS_SaveSecretKey_' + windowNo + '" class="VIS-save-secret-key-copy-btn">Copy</button>' +
+            '<button id="VIS_SaveSecretKey_' + windowNo + '" class="VIS-save-secret-key-copy-btn">' + VIS.Msg.getMsg("VAAPI_Copy") + '</button>' +
             '</div>' +
-            '<button id="VIS_DoneSecretKey_' + windowNo + '" class="VIS-save-secret-key-done-btn">Done</button>' +
+            '<button id="VIS_DoneSecretKey_' + windowNo + '" class="VIS-save-secret-key-done-btn">' + VIS.Msg.getMsg("VAAPI_Done") + '</button>' +
             '</div>';
 
 
@@ -41,6 +41,8 @@
                     //'<h2>' + VIS.Msg.getMsg("VAAPI_EditSecretKey") + '</h2>' +
                     '<label for="keyName">' + VIS.Msg.getMsg("VAAPI_KeyName") + ' </label>' +
                     '<input type="text" id="VIS_EditSecretKeytxt_' + windowNo + '" placeholder="My Test Key"  value="' + keyName + '">' +
+                    '<div id="VIS_PasswordError_' + windowNo + '" class="VIS-key-error-message"></div>' +
+
                     '<label for="project">' + VIS.Msg.getMsg("VAAPI_Project") + '</label>' +
                     '<select id="VIS_ProjectSelect_' + windowNo + '">' +
                     '<option>Default project</option>' +
@@ -63,13 +65,13 @@
 
             } else {
                 return (
-                    '<div id="VIS_PasswordDiv_' + windowNo + '" class="VIS-key-confirm-password">' +
-                    '<div class="">' +
+                    '<div id="VIS_PasswordDiv_' + windowNo + '" class="VIS-confirm-password-container">' +
+                    '<div   class="VIS-key-confirm-password">' +
                     //'<h2 class="VIS-popup-header">' + VIS.Msg.getMsg("VAAPI_EnterYourPassword") + '</h2>' +
-                    '<p class="VIS-popup-description">' + VIS.Msg.getMsg("VAAPI_EnterYourPasswordInfo") + '</p>' +
+                    '<p class="VIS-popup-description VIS-secret-key-p-content">' + VIS.Msg.getMsg("VAAPI_EnterYourPasswordInfo") + '</p>' +
                     '<input id="VIS_Passwordtxt_' + windowNo + '" type="password" placeholder="Enter your password" class="VIS-popup-input">' +
-                    '<button id="VIS_ConfirmPassword_' + windowNo + '" class="VIS-popup-button">Done</button>' +
-                    '<div id="VIS_PasswordError_' + windowNo + '" class="VIS-error-message" style="display:none; color: red; font-size: 14px;"></div>' +
+                    '<button id="VIS_ConfirmPassword_' + windowNo + '" class="VIS-popup-button">' + VIS.Msg.getMsg("VAAPI_Done") + '</button>' +
+                    '<div id="VIS_PasswordError_' + windowNo + '" class="VIS-key-error-message"></div>' +
                     '</div>' +
                     '</div>'
                 );
@@ -84,11 +86,19 @@
         // Event listener for the Save or Confirm button based on mode
         if (mode) {
             $root.find('#VIS_SaveEditedSecretKey_' + windowNo).on("click", function () {
+                $root.find('#VIS_PasswordError_' + windowNo).hide();
                 $bsyDiv[0].style.visibility = 'visible';
                 var keyName = $root.find('#VIS_EditSecretKeytxt_' + windowNo).val();
                 var projectID = $root.find('#VIS_ProjectSelect_' + windowNo).val();
                 var isActiveChecked = $root.find('#VIS_ActiveToggle_' + windowNo).is(":checked");
+                // Validation: Check if keyName is empty
+                if (!keyName) {
+                    $root.find('#VIS_PasswordError_' + windowNo).text(VIS.Msg.getMsg("VAAPI_KeyNameMandatory")).show();
+                    $root.find('#VIS_EditSecretKeytxt_' + windowNo).focus(); // Focus back on input field
+                    $bsyDiv[0].style.visibility = 'hidden';
 
+                    return; // Stop further execution
+                }
                 const updateData = {
                     KeyName: keyName,
                     ProjectID: projectID,
@@ -103,7 +113,7 @@
                     data: JSON.stringify(updateData),
                     success: function (response) {
                         if (response) {
-                          //VIS.ADialog.info("Secret Key updated successfully!");
+                            //VIS.ADialog.info("Secret Key updated successfully!");
                             $bsyDiv[0].style.visibility = 'hidden';
                             $self.onClose();
                             ch.close();
@@ -129,7 +139,7 @@
                 $bsyDiv[0].style.visibility = 'visible';
                 var password = $root.find('#VIS_Passwordtxt_' + windowNo).val().trim();
                 if (password === "") {
-                    $root.find('#VIS_PasswordError_' + windowNo).text("please enter your password").show();
+                    $root.find('#VIS_PasswordError_' + windowNo).text(VIS.Msg.getMsg("VAAPI_PleaseEnterPassword")).show();
                     $bsyDiv[0].style.visibility = 'hidden';
                     return;
                 }
@@ -157,7 +167,11 @@
                             $root.append(saveKeyHtml); // Show Save Key UI
                             $root.find('#VIS_SecretKeytxt_' + windowNo).val(response.sessionToken); // Set session token
                             $bsyDiv[0].style.visibility = 'hidden';
-                            ch.changeHeight(260);
+                            setTimeout(function () {
+                                var contentHeight = $root.find('.VIS-key-popup-container')[0].offsetHeight + 6;
+                                ch.changeHeight(contentHeight);
+                            }, 50);
+                            ch.changeWidth(657);
                             ch.changeTitle(VIS.Msg.getMsg("VAAPI_SaveYourKey"));
                             // Add event listener to "Copy" button
                             $root.find('#VIS_SaveSecretKey_' + windowNo).on("click", function () {
@@ -168,27 +182,32 @@
                                     alert('The textbox is empty!');
                                     return;
                                 }
+                                // Create a temporary textarea element to copy text
+                                var tempTextArea = $('<textarea>');
+                                $root.append(tempTextArea);
+                                tempTextArea.val(secretKeyText);
 
-                                // Use Clipboard API to copy the secret key
-                                navigator.clipboard.writeText(secretKeyText)
-                                    .then(function () {
-                                        var $copyButton = $root.find('#VIS_SaveSecretKey_' + windowNo);
-                                        $copyButton.html('✔️'); // Replace button text with tick icon
-                                        $copyButton.prop('disabled', true); // Disable the button
+                                // Select the text properly
+                                tempTextArea[0].select();
+                                tempTextArea[0].setSelectionRange(0, tempTextArea.val().length); // Explicit selection
 
-                                        // Set timeout to revert back to the original button after 5 seconds
-                                        setTimeout(function () {
-                                            $copyButton.html('Copy'); // Revert back to "Copy"
-                                            $copyButton.prop('disabled', false); // Enable the button again
-                                        }, 5000);
-                                     /*  alert('Secret key copied: ' + secretKeyText);*/
-                                    })
-                                    .catch(function (err) {
-                                        console.error('Error copying text: ', err);
-                                        alert('Error copying text');
-                                    });
+                                try {
+                                    document.execCommand('copy'); // Copy text
+                                    var $copyButton = $root.find('#VIS_SaveSecretKey_' + windowNo);
+                                    $copyButton.html('<span class="VIS-copy-tick-icon">✔️</span>');
+                                    $copyButton.prop('disabled', true); // Disable the button
 
+                                    // Set timeout to revert back to the original button after 5 seconds
+                                    setTimeout(function () {
+                                        $copyButton.html(VIS.Msg.getMsg("VAAPI_Copy")); // Revert back to "Copy"
+                                        $copyButton.prop('disabled', false); // Enable the button again
+                                    }, 5000);
 
+                                } catch (err) {
+                                    console.error('Error copying text: ', err);
+                                }
+
+                                tempTextArea.remove(); // Remove temporary textarea
                             });
 
                             $root.find('#VIS_DoneSecretKey_' + windowNo).on("click", function () {
@@ -220,7 +239,7 @@
                 success: function (data) {
                     $self.projectList = JSON.parse(data);
                     $root.find();
-                    var cmbProjectContent = "<option value=\"\"></option>";
+                    var cmbProjectContent = "";
                     for (var itm in $self.projectList) {
                         cmbProjectContent += "<option value=" + $self.projectList[itm].RecKey + ">" + $self.projectList[itm].Name + "</option>";
                     }
@@ -231,14 +250,16 @@
             });
 
         };
+
+
         this.show = function () {
             var windowWidth = $(window).width();
             if (windowWidth < 768) {
-                windowWidth = '40%';
+                windowWidth = '50';
             } else if (windowWidth >= 768 && windowWidth < 900) {
-                windowWidth = '50%';
+                windowWidth = '60';
             } else {
-                windowWidth = '20%';
+                windowWidth = '30';
             }
             load();
             ch = new VIS.ChildDialog();
@@ -246,9 +267,16 @@
                 ch.setHeight(405);
             }
             else {
-                ch.setHeight(280);
+                setTimeout(function () {
+                    var contentHeight = $root.find('.VIS-confirm-password-container')[0].offsetHeight + 6;
+                    ch.setHeight(contentHeight);
+                }, 50);
             }
-            ch.setWidth(windowWidth);
+            if (mode) {
+                windowWidth -= 10;
+            }
+            var finalWidth = windowWidth + '%';
+            ch.setWidth(finalWidth);
             ch.setTitle(mode ? VIS.Msg.getMsg("VAAPI_EditSecretKeyHeader") : VIS.Msg.getMsg("VAAPI_ConfirmPasswordHeader"));
             ch.setModal(true);
             ch.setEnableResize(false);
