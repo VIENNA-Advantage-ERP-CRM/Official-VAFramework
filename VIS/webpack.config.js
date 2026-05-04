80812d000150ba218567b775bd009461abdd614a
Yousif <yousifalfalahy@gmail.com>
2026-05-04 16:27:57 +0300

    Revert "Add the Invoices Widget + Page"
    This reverts commit 46958fa3e52b5081063b607f3710457088026ece.

--- VIS/webpack.config.js
+++ VIS/webpack.config.js
@@ -19,7 +19,7 @@ deleteFilesByPattern(path.resolve(__dirname, 'Areas/VIS/Content'), /^VIS\.all\.m
 
 
 const versions = {
-    'VIS.all': '3.0.2.12',
+    'VIS.all': '3.0.2.11',
     'VIS2_0': '3.0.2.11',
     'React': '1.0.0.1',
     'VIS': '3.0.2.11' // CSS Version
