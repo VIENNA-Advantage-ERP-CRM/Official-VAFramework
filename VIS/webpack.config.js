13168070dd6a08bf10ecd2e5975d6bc6187a28e4
liveserver <live@viennasolutions.com>
2026-08-19 08:28:26 +0000

    Version Update

--- VIS/webpack.config.js
+++ VIS/webpack.config.js
@@ -19,10 +19,10 @@ deleteFilesByPattern(path.resolve(__dirname, 'Areas/VIS/Content'), /^VIS\.all\.m
 
 
 const versions = {
-    'VIS.all': '3.1.81.0',
-    'VIS2_0': '3.1.81.0',
+    'VIS.all': '3.1.84.0',
+    'VIS2_0': '3.1.84.0',
     'React': '1.0.0.1',
-    'VIS': '3.1.62.0' // CSS Version
+    'VIS': '3.1.65.0' // CSS Version
 };
 
 module.exports = {
